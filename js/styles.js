19c8aa66bedca95cc24512684770a7cf31c8f0e3
Snezhana Pashovska <snezhana.pashovska@hotmail.com>
2022-04-01 09:33:01 +0200

    Update JavaScript

--- js/styles.js
+++ js/styles.js
@@ -3,7 +3,7 @@ const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu(){
+function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 };
@@ -13,6 +13,6 @@ const navLink = document.querySelectorAll(".nav-link");
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
